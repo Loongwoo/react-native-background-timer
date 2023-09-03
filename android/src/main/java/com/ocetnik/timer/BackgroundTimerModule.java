bfeb1f27635a7f3919dbfe95b3e08ca34bb95e68
lyswhut <lyswhut@qq.com>
2023-09-03 14:21:37 +0800

    Remove wakeLock

--- android/src/main/java/com/ocetnik/timer/BackgroundTimerModule.java
+++ android/src/main/java/com/ocetnik/timer/BackgroundTimerModule.java
@@ -15,32 +15,14 @@ import java.lang.Runnable;
 public class BackgroundTimerModule extends ReactContextBaseJavaModule {
 
     private Handler handler;
-    private ReactContext reactContext;
+    private final ReactContext reactContext;
     private Runnable runnable;
-    private PowerManager powerManager;
-    private PowerManager.WakeLock wakeLock;
-    
+
     private int listenerCount = 0;
-
-    private final LifecycleEventListener listener = new LifecycleEventListener(){
-        @Override
-        public void onHostResume() {}
-
-        @Override
-        public void onHostPause() {}
-
-        @Override
-        public void onHostDestroy() {
-            if (wakeLock.isHeld()) wakeLock.release();
-        }
-    };
 
     public BackgroundTimerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        this.powerManager = (PowerManager) getReactApplicationContext().getSystemService(reactContext.POWER_SERVICE);
-        this.wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "rohit_bg_wakelock");
-        reactContext.addLifecycleEventListener(listener);
     }
 
     @Override
@@ -48,7 +30,6 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
         return "RNBackgroundTimer";
     }
 
-    
     @ReactMethod
     public void addListener(String eventName) {
         if (listenerCount == 0) {
@@ -68,23 +49,14 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void start(final int delay) {
-        if (!wakeLock.isHeld()) wakeLock.acquire();
-
         handler = new Handler();
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                sendEvent(reactContext, "backgroundTimer");
-            }
-        };
+        runnable = () -> sendEvent(reactContext, "backgroundTimer");
 
         handler.post(runnable);
     }
 
     @ReactMethod
     public void stop() {
-        if (wakeLock.isHeld()) wakeLock.release();
-
         // avoid null pointer exceptio when stop is called without start
         if (handler != null) handler.removeCallbacks(runnable);
     }
@@ -98,16 +70,13 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void setTimeout(final int id, final double timeout) {
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                if (getReactApplicationContext().hasActiveCatalystInstance()) {
-                    getReactApplicationContext()
-                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                        .emit("backgroundTimer.timeout", id);
-                }
-           }
-        }, (long) timeout);
+        handler.postDelayed(() -> {
+            if (reactContext.hasActiveReactInstance()) {
+                reactContext
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("backgroundTimer.timeout", id);
+            }
+       }, (long) timeout);
     }
 
     /*@ReactMethod
